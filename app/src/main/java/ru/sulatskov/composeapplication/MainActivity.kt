f3d1794e73e2038ea0f1ad1da8895feb5995bcdc
Sulatskov Sergey <sulatskov.s@r-mades.com>
2020-12-25 15:35:07 +0400

    init

---
+++ app/src/main/java/ru/sulatskov/composeapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.sulatskov.composeapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
